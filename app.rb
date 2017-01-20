eb94cd7e017996df31421e7361315bb8ec535bdd
杉山 <yuu_16n1200018@nnn.ed.jp>
2017-01-20 15:55:57 +0900

    install sinatra and slim

---
+++ app.rb
@@ -0,0 +1,22 @@
+require "bundler/setup"
+Bundler.require(:default)
+require "sinatra/reloader"
+
+class App < Sinatra::Base
+  configure do
+    register Sinatra::Reloader
+  end
+
+  configure do
+    set :views, settings.root + "/views"
+  end
+
+  get "/" do
+    slim :index
+  end
+
+  get "/:name" do
+    @name = params[:name]
+    slim :index
+  end
+end
